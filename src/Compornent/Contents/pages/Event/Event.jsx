9d68a908a24486feee513272c539e7861efcdb49
Leehg5 <kimo930117@naver.com>
2022-08-25 20:07:39 +0900

    이벤트

--- src/Compornent/Contents/pages/Event/Event.jsx
+++ src/Compornent/Contents/pages/Event/Event.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import Heading from "./Heading";
+import styles from "./Event.module.css";
 import Menu01 from "./Menu01";
-import Menu02 from "./Menu02";
-import Menu03 from "./Menu03";
-import { Routes, Route } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
 
 const Event = () => {
   return (
     <div>
-      
       <Heading />
-      <Routes>  
-    <Route path="/Menu01" element={<Menu01 />}/>
-    <Route path="/Menu02" element={<Menu02 />}/>
-    <Route path="/Menu03" element={<Menu03 />}/>
-  </Routes>
-  </div>
-  ) 
-  
+      <Outlet />
+    </div>
+  );
 };
 
 export default Event;
--- src/Compornent/Contents/pages/Event/Menu02.jsx
+++ src/Compornent/Contents/pages/Event/Menu02.jsx
@@ -1,45 +1,81 @@
-import React from 'react';
+import React from "react";
+import styles from "./Event.module.css";
 
 function Menu02() {
   return (
     <div>
-      <div className='pi'>
-        <img className='pi1' src="https://www.gong-cha.co.kr/uploads/board/20220805/yPK6NXUDzr2ILbpC_20220805.jpg" 
-        alt="" />
-        <div className='pi2'>
-        <p className='name1'>요기요 4,000원 할인
-        <span className='span1'>종료</span></p>
-        <p className='name2'>2022년 08월 08일(월) ~ 2022년 08월 14일(일)</p>
-        <p><a className='name3' href="">자세히 보기  {'>'}</a></p>
-        </div></div>
-        <p className='cc'></p>
-        <div className='pi'>
-        <img className='pi1' src="https://www.gong-cha.co.kr/uploads/board/20220801/x4YKXblsG9MhStLa_20220801.jpg" 
-        alt="" />
-        <div className='pi2'>
-        <p className='name1'>스페셜 커피 신메뉴 마시면, 스탬프 x3배 적립!
-        <span className='span1'>종료</span></p>
-        <p className='name2'>2022년 08월 04일(목) ~ 2022년 08월 14일(일)</p>
-        <p><a className='name3' href="">자세히 보기 {'>'}</a></p>
-        </div></div>
-        <p className='cc'></p>
-        <div className='pi'>
-        <img className='pi1' src="https://www.gong-cha.co.kr/uploads/board/20220729/RX3YluOi94FsqrVc_20220729.jpg" 
-        alt="" />
-        <div className='pi2'>
-        <p className='name1'>배달의 민족 공차 3,000원 할인
-        <span className='span1'>종료</span></p>
-        <p className='name2'>2022년 08월 01일(월) ~ 2022년 08월 07일(일)</p>
-        <p><a className='name3' href="">자세히 보기  {'>'}</a></p>
-        </div></div>
-        <p className='cc'></p>
-        <div className='boutton1'>
-        <button className='button2'> 더보기 </button>
-        
+      <div className={styles.Leediv1}>
+        <div className={styles.Leedivpi1}>
+          <img
+            className={styles.Leeimg12}
+            src="https://www.gong-cha.co.kr/uploads/board/20220809/uPCk1jKRS8eglsJ7_20220809.jpg"
+            alt=""
+          />
+          <div className={styles.Leedivpi23}>
+            <p className={styles.Leepp}>
+              학생증만 있으면, 제조 음료 구매 시 블랙/타로 밀크티 1,000원!
+            </p>
+            <p className={styles.Leepp3}>
+              2022년 08월 12일(금) ~ 2022년 08월 26일(금)
+            </p>
+            <p>
+              <a className={styles.Leepp1} href="">
+                자세히 보기 {">"}
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className={styles.Leeasdasd}>s</div>
+      </div>
+      <div className={styles.Leediv1}>
+        <div className={styles.Leedivpi1}>
+          <img
+            className={styles.Leeimg12}
+            src="https://www.gong-cha.co.kr/uploads/board/20220805/yPK6NXUDzr2ILbpC_20220805.jpg"
+            alt=""
+          />
+          <div className={styles.Leedivpi23}>
+            <p className={styles.Leepp}>요기요 4,000원 할인</p>
+            <p className={styles.Leepp3}>
+              2022년 08월 08일(월) ~ 2022년 08월 14일(일)
+            </p>
+            <p>
+              <a className={styles.Leepp1} href="">
+                자세히 보기 {">"}
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className={styles.Leeasdasd}>s</div>
+      </div>
+      <div className={styles.Leediv1}>
+        <div className={styles.Leedivpi1}>
+          <img
+            className={styles.Leeimg12}
+            src="https://www.gong-cha.co.kr/uploads/board/20220801/x4YKXblsG9MhStLa_20220801.jpg"
+            alt=""
+          />
+          <div className={styles.Leedivpi23}>
+            <p className={styles.Leepp}>
+              스페셜 커피 신메뉴 마시면, 스탬프 x3배 적립!종료
+            </p>
+            <p className={styles.Leepp3}>
+              2022년 08월 04일(목) ~ 2022년 08월 14일(일)
+            </p>
+            <p>
+              <a className={styles.Leepp1} href="">
+                자세히 보기 {">"}
+              </a>
+            </p>
+          </div>
+        </div>
+        <div className={styles.Leeasdasd}>s</div>
+        <p className={styles.Leecc}></p>
+        <div className={styles.LeeButton1}>
+          <button className={styles.LeeButton2}> 더보기 </button>
         </div>
       </div>
-    
+    </div>
   );
-  
 }
 export default Menu02;
